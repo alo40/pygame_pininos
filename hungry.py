5e789a1e2aa895a86dea36c90cffb915cf9bae1b
unknown <alejandronieto40@gmail.com>
2021-12-25 23:23:18 -0600

    added hungry prompt

--- hungry.py
+++ hungry.py
@@ -5,5 +5,8 @@ Created on Sat Dec 25 22:54:33 2021
 @author: fori
 """
 
-
-print("I'm hungry")
+hungry=input("are you hungry?")
+if hungry=="yes":
+    print("eat taquitos")
+else:
+    print("do some sport")
