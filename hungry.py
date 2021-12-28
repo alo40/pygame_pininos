9e1d8d0b627cc17c8ab3ae59ecf83b7b61142dfa
unknown <alejandronieto40@gmail.com>
2021-12-28 10:46:36 -0600

    added enchiladas

--- hungry.py
+++ hungry.py
@@ -1,13 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec 25 22:54:33 2021
-
-@author: fori
-"""
-
-hungry=input("are you hungry?")
-if hungry=="yes":
-    print("eat taquitos")
-    print("eat nachos")
+hungry=input('are you hungry?')
+if hungry=='yes':
+    print('eat taquitos')
+    print('eat nachos')
+    print('eat enchiladas')
 else:
-    print("do some sport")
+    print('do some codding')
