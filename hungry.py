74c4dc474a746429a5e8584a7276b573f6a48c76
unknown <alejandronieto40@gmail.com>
2021-12-28 11:06:16 -0600

    added thirsty prompt

--- hungry.py
+++ hungry.py
@@ -4,4 +4,8 @@ if hungry=='yes':
     print('eat nachos')
     print('eat enchiladas')
 else:
-    print('do some codding')
+    thirsty=input('are you thirsty?')
+    if thirsty=='yes':
+        print('drink water')
+    else:
+        print('do nothing')
