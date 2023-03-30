3c72dc3b2b12d155a445f7a196321e1fb70ea6f1
alo40 <alejandronieto40@gmail.com>
2023-03-30 18:24:17 +0200

    added main file

---
+++ main_pininos.py
@@ -0,0 +1,11 @@
+import pygame as pg
+
+pg.init()
+width = 800
+height = 400
+screen = pg.display.set_mode((width, height))
+
+while True:
+    # draw all our elements
+    # update everything
+    pg.display.update()
