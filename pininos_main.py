0c3c6171c8c721becf52633b9e9d9e6f1ca4990d
alo40 <alejandronieto40@gmail.com>
2023-04-18 21:55:21 +0200

    added GNU General Public License

--- pininos_main.py
+++ pininos_main.py
@@ -1,3 +1,24 @@
+""" 
+    Pininos' game is about compromises and jumps, enjoy it!
+    Copyright (C) 2023  Alejandro Nieto Cuarenta
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact email: alejandronieto40@gmail.com
+"""
+
+
 import pygame as pg
 from sys import exit
 from random import randint, choice  # choice will be used to random spawn different types of enemies
